6e7929e56f7f677c762995247d1b9a9bc4eb48c8
Dimas Pradipto <dimaspradipto11@gmail.com>
2022-04-22 10:33:25 +0700

    updata list kategory

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -1,14 +1,54 @@
 import React, { Component } from "react";
-import { Col } from "react-bootstrap";
+import { Col, ListGroup } from "react-bootstrap";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faMugHot,
+  faCheese,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Icon = ({ nama }) => {
+  if (nama === "Makanan") return <FontAwesomeIcon icon={faUtensils} />;
+  if (nama === "Minuman") return <FontAwesomeIcon icon={faMugHot} />;
+  if (nama === "Cemilan") return <FontAwesomeIcon icon={faCheese} />;
+};
 
 export default class ListCategories extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(API_URL + "categories").then((res) => {
+      const categories = res.data;
+      this.setState({ categories });
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <Col md={2}>
         <h4>
           <strong>Daftar Kategori</strong>
         </h4>
         <hr />
+        <ListGroup>
+          {categories &&
+            categories.map((category) => (
+              <ListGroup.Item>
+                <h5>
+                  <Icon nama={category.nama} /> {category.nama}
+                </h5>
+              </ListGroup.Item>
+            ))}
+        </ListGroup>
       </Col>
     );
   }
